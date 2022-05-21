6e30c12360a708568b3654ed317316da3879d70e
PatrickReis <preis@fi.uba.ar>
2022-05-21 19:19:44 -0300

    Add pinata keys

--- backend/src/PinataKeys.ts
+++ backend/src/PinataKeys.ts
@@ -1,4 +1,4 @@
 export default {
-  apiKey: "DUMMY_KEY",
-  apiSecret: "DUMMY_SECRET"
+  apiKey: "5a0ac8c747acb98e3693",
+  apiSecret: "451a6493377e791bbea07636e41278ab75ece983c1368532f40f3a19c251d1b3"
 };
